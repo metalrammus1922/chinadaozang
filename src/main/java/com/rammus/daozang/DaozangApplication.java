9f06f385d8ce10bf3a3bd997cf9f634daf1b177c
chenkaiss <chenkaiss@epoint.com.cn>
2025-03-09 17:47:37 +0800

    第一次提交

---
+++ src/main/java/com/rammus/daozang/DaozangApplication.java
@@ -0,0 +1,13 @@
+package com.rammus.daozang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaozangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaozangApplication.class, args);
+	}
+
+}
