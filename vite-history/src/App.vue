770bc20a6627816b814fc149f8c8a950ab1aae6f
metalrammus1922 <metalrammus@icloud.com>
2026-02-15 11:04:02 +0800

    [fix]集成axios

--- vite-history/src/App.vue
+++ vite-history/src/App.vue
@@ -16,14 +16,7 @@
         <a-menu-item key="3">nav 3</a-menu-item>
       </a-menu>
     </a-layout-header>
-    <a-layout-content style="padding: 0 50px">
-      <a-breadcrumb style="margin: 16px 0">
-        <a-breadcrumb-item>Home</a-breadcrumb-item>
-        <a-breadcrumb-item>List</a-breadcrumb-item>
-        <a-breadcrumb-item>App</a-breadcrumb-item>
-      </a-breadcrumb>
-      <div :style="{ background: '#fff', padding: '24px', minHeight: '280px' }">Content</div>
-    </a-layout-content>
+    <router-view />
     <a-layout-footer style="text-align: center">
       Ant Design ©2018 Created by Ant UED
     </a-layout-footer>
